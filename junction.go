f7f6cea9b6a1a42c610480fa39f77a21c2a16c81
David Brunton <dbrunton@gmail.com>
2011-11-07 08:03:46 -0500

    stub working version

--- junction.go
+++ junction.go
@@ -1,5 +1,30 @@
 package junction
 
-func Junction(junc func() bool, evalue bool, nevalue int) (bool) {
- return false
+import (
+	"time",
+	"fmt",
+)
+
+func all(junc ...func()(bool)) bool {
+
+        c := make(chan bool)
+        t := make(chan bool)
+
+        go func() { time.Sleep(5e9); t <- true }()
+
+        for _, f := range junc {
+                r := f()
+                go func() { c <- r }()
+        }
+
+        for {
+                select {
+                        case j := <-c:
+                                // should probably have a case where everything's been received.
+                                if !j { return false }
+                        case <-t:
+                                return true
+                }
+        }
+        return true
 }
--- junction_test.go
+++ junction_test.go
@@ -4,15 +4,11 @@ import(
  "testing"
 )
 
-var junctionTests = []bool{
- false,
-}
+func rett()(bool) { return true }
+func retf()(bool) { return false }
 
 func TestJunction(t *testing.T) {
- for _, jt := range junctionTests {
-  result := Junction()
-  if result != false {
-   t.Errorf("Prompt is wrong: '%s'\n", jt)
-  }
- }
+	if all(rett, rett, retf) {
+		t.Errorf("all(rett, rett, retf) evaluated true, should be false.")
+	}
 }
