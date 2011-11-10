dc9312ab7a1f997d6866af048310649acc198bea
David Brunton <dbrunton@gmail.com>
2011-11-10 10:41:48 -0500

    added a few more tests.

--- junction.go
+++ junction.go
@@ -4,18 +4,18 @@ import "time"
 
 func all(junc ...func()(bool)) bool {
 
-        c := make(chan bool)
+        retc := make(chan bool)
         t := make(chan bool)
 
         go func() { time.Sleep(1e10); t <- true }()
 
         for _, f := range junc {
-		go one(f, c)
+		go one(f, retc)
         }
 
         for i := 0; i < cap(junc); i++ {
                 select {
-                        case j := <-c:
+                        case j := <-retc:
                                 if j != true { return false }
                         case <-t:
                                 return true
--- junction_test.go
+++ junction_test.go
@@ -1,16 +1,25 @@
 package junction
 
 import(
-// "time"
+ "time"
  "testing"
 )
 
 func rett()(bool) { return true }
 func retf()(bool) { return false }
+func retfWait()(bool) { time.Sleep(5e8); return false }
+func rettWait()(bool) { time.Sleep(5e8); return true }
 
 func TestJunction(t *testing.T) {
 	if all(retf, rett) {
 		t.Errorf("all(retf, rett) evaluated true, should be false.")
+	}
+
+	if all(rett, rett, rett, rett) { } else {
+		t.Errorf("all(rett, rett, rett, rett) returned false, should be true.")
+	}
+	if all(rettWait, rettWait, retf) {
+		t.Errorf("Failed with waiting.")
 	}
 
 }
